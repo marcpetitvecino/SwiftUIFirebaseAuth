cf9a5250e9a520f20e47b654b0ef82332aa0923c
marcpetitvecino <marcpetitvecino@gmail.com>
2022-06-24 17:36:02 +0200

    Initial Commit

---
+++ SwiftUIFirebaseAuth/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIFirebaseAuth
+//
+//  Created by Marc Petit Vecino on 24/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIFirebaseAuth/SwiftUIFirebaseAuthApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFirebaseAuthApp.swift
+//  SwiftUIFirebaseAuth
+//
+//  Created by Marc Petit Vecino on 24/6/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFirebaseAuthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
